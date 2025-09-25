685e9fabf2499f78f3285ab77801b91799fcf4bb
Syed-Urwah <syedurwahdev@gmail.com>
2025-09-25 18:35:17 +0500

    set swagger docs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { initializeTransactionalContext, addTransactionalDataSource } from 'typeorm-transactional';
 import { DataSource } from 'typeorm';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -22,6 +23,13 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') ?? 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Auto Accounting API')
+    .setDescription('The Auto Accounting API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}/api`);
 }
